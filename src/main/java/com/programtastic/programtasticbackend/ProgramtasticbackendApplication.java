57738fa57a53815d00ab011418197df0e650a91a
jholder7 <jholder7@murraystate.edu>
2024-02-09 17:02:41 -0600

    init

---
+++ src/main/java/com/programtastic/programtasticbackend/ProgramtasticbackendApplication.java
@@ -0,0 +1,13 @@
+package com.programtastic.programtasticbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramtasticbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramtasticbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programtastic/programtasticbackend/ProgramtasticbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programtastic.programtasticbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramtasticbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
